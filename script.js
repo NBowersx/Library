1d84f28ded63e90ff6217f246a076a5994600876
Nathan <Bowers.Nathanx@gmail.com>
2024-03-26 23:23:45 -0400

    add read status

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const myLibrary = [];
 
 
 //add book proto
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   // the constructor...
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
     this.info = function(){
         return(this.title+' by '+this.author+' is '+this.pages+'pages.')
     }
@@ -14,14 +15,16 @@ function Book(title, author, pages) {
 
 //updates library 
 function addBookToLibrary() {
+
+  //get all input info
   const title = document.getElementById('enterTitle').value;
   const author = document.getElementById('enterAuthor').value;
   const pages = document.getElementById('enterPages').value;
-  const read = document.getElementById('enterRead').value;//fix
-  const newBook = new Book(title, author, pages);
+  const read = document.getElementById('enterRead').checked;//fix
+  console.log(read)
+  
+  const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook)
-  console.log(newBook.info())
-  console.log(title, author, pages);
   updateLibrary();
 }
 
@@ -34,24 +37,60 @@ function updateLibrary(){
         const bookDiv = document.createElement("div");
         bookDiv.classList.add('book');
         bookDiv.addEventListener('click', ()=> openBook(bookDiv));
+
+        const images = ['dune.jpg', 'alc.jpg', 'gatsby.jpg', '1984.jpeg', 'invis.jpg', 'outlawed.jpg'];
+        const randomImage = images[Math.floor(Math.random() * images.length)];
        
+        const yesOrNO = book.read ? 'yes.png' : 'no.jpg'
+        console.log('below')
+        console.log(book.read)
+        console.log(book.title)
+        console.log(book.author)
+        
+
+
         bookDiv.innerHTML = `
             <div class="bookCoverWrap">
-                <img src="./imgs/dune.jpg" alt="Book Cover">
+                <img src="./imgs/${randomImage}" alt="Book Cover">
             </div>
             <div class="info">
                 <div class="title">${book.title}</div>
                 <div class="author">by ${book.author}</div>
                 <div class="pages">${book.pages}</div>
+                <button type="button" class="delete" onclick="deleteBook()">Delete</button>
+            </div>
+            <div class="read">
+                <img src="./imgs/${yesOrNO}" alt="book has been read">
             </div>
         `;
         bookLibrary.appendChild(bookDiv);
     });
        
 }
+//https://covers.openlibrary.org/b/isbn/${book.isbn}-L.jpg
 
-//add unique class to all divs in clicked book
+
+//add unique class to all divs in clicked book make new div ssssss
+//maybe look into making it a dialog
 function openBook(bookDiv){
- console.log('fde');
- bookDiv.classList.add('clicked')
+ console.log('clicked');
+ const clonedBookDiv = bookDiv.cloneNode(true);
+ clonedBookDiv.classList.add('clicked')
+
+ document.body.appendChild(clonedBookDiv);
+ event.stopPropagation();
+ 
+
+}
+
+function closeBook(){
+    var elements = document.querySelectorAll('.clicked');
+    elements.forEach(function(element) {
+        element.remove();
+    });
+}
+
+function deleteBook(){
+    console.log('deleted')
+
 }
